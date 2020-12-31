eba85a1202409e841e52673faef538e59cb808c5
MarkArren <m.arrenburg@gmail.com>
2020-12-31 17:40:29 +0000

    Nav bar now mobile friendly

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,52 +1,76 @@
 import './Navbar.scss';
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { FiMessageCircle } from 'react-icons/fi';
 import { MdNotifications } from 'react-icons/md';
-import { AiOutlineUser } from 'react-icons/ai';
+import { AiOutlineUser, AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 import { IoAddCircleOutline } from 'react-icons/io5';
 import { IconContext } from 'react-icons';
 
 function Navbar() {
+    const [navMobileOpen, setNavMobileOpen] = useState(false);
+    const searchRef = useRef();
+
+    function handleSearch(e) {
+        e.preventDefault();
+        console.log(searchRef.current.value);
+        return searchRef.current.value;
+    }
+
     return (
-        <IconContext.Provider
-            value={{
-                color: 'hsl(0,0%, 25%)',
-                size: '40px',
-                style: { fill: 'hsl(0,0%, 25%)', verticalAlign: 'middle' },
-                className: 'global-class-name',
-            }}
-        >
-            <div className='navbar'>
-                <Link to='/' className='nav-left'>
-                    <h3>ProjectName</h3>
+        <div className='navbar'>
+            <Link to='/' className='nav-left'>
+                <h3>ProjectName</h3>
+            </Link>
+            <div className='nav-right'>
+                <form onSubmit={handleSearch}>
+                    <input
+                        type='text'
+                        ref={searchRef}
+                        placeholder='Search'
+                        className='nav-search'
+                    />
+                </form>
+                <Link to='/upload' className='nav-icon'>
+                    <IoAddCircleOutline />
                 </Link>
-                <div className='nav-right'>
-                    <div className='nav-seach nav-icon '>
-                        <form className='nav-seach-form '>
-                            <input
-                                type='text'
-                                name='search'
-                                placeholder='Search'
-                                className='nav-search'
-                            />
-                        </form>
-                    </div>
-                    <Link to='/upload' className='nav-profile nav-icon'>
-                        <IoAddCircleOutline />
-                    </Link>
-                    <Link to='/messages' className='nav-messages nav-icon'>
-                        <FiMessageCircle />
-                    </Link>
-                    <div className='nav-notif nav-icon'>
-                        <MdNotifications />
-                    </div>
-                    <Link to='/profile' className='nav-profile nav-icon'>
-                        <AiOutlineUser />
-                    </Link>
+                <Link to='/messages' className='nav-icon'>
+                    <FiMessageCircle />
+                </Link>
+                <div className='nav-icon'>
+                    <MdNotifications />
+                </div>
+                <Link to='/profile' className='nav-icon'>
+                    <AiOutlineUser />
+                </Link>
+            </div>
+            <div className='nav-right-mobile'>
+                <div className='nav-icon'>
+                    <MdNotifications />
+                </div>
+                <div className='nav-mobile-toggle nav-icon'>
+                    {navMobileOpen ? (
+                        <AiOutlineClose
+                            onClick={() => {
+                                setNavMobileOpen(!navMobileOpen);
+                            }}
+                        />
+                    ) : (
+                        <AiOutlineMenu
+                            onClick={() => {
+                                setNavMobileOpen(!navMobileOpen);
+                            }}
+                        />
+                    )}
+                </div>
+                <div className={navMobileOpen ? 'nav-mobile' : 'hide'}>
+                    <Link to='/'>Home</Link>
+                    <Link to='/upload'>Upload</Link>
+                    <Link to='/messages'>Messages</Link>
+                    <Link to='/profile'>Profile</Link>
                 </div>
             </div>
-        </IconContext.Provider>
+        </div>
     );
 }
 
